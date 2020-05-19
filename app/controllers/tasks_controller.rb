ac03bb7d5d7fe54bbedfda456d217af33618a20a
EC2 Default User <ec2-user@ip-172-31-39-30.ap-northeast-1.compute.internal>
2020-05-19 01:54:33 +0000

    C6途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,27 @@
 class TasksController < ApplicationController
+  before_action :set_user
   
+    def index
+        @tasks = @user.tasks
+    end
+    
+    def show
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+    end
+    
+    private
 
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
+    
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,4 +24,8 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
